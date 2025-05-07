811a5b8c32449fc047979c255bba69b3924e29ba
Boniface <bm715452@gmail.com>
2025-05-07 08:53:22 +0300

    comprehension tests

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
+    if not isinstance(num_list, (list, tuple, range)):
+        raise TypeError("Input must be a sequence")
+    return [num for num in num_list if num % 2 == 0]
     pass
 
 def make_exclamation(sentence_list):
+    if not isinstance(sentence_list, (list, tuple)):
+        raise TypeError("Input must be a list or string")
+    return [sentence + "!" for sentence in sentence_list]
     pass
